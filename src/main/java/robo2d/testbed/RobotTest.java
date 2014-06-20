03c623503e8729033b12b478a1dd8719ec3d9dad
xaos <xaos@z-inc.net>
2014-06-20 21:47:04 +0600

    Initial commit

---
+++ src/main/java/robo2d/testbed/RobotTest.java
@@ -0,0 +1,63 @@
+package robo2d.testbed;
+
+import org.jbox2d.common.Vec2;
+import org.jbox2d.testbed.framework.*;
+import org.jbox2d.testbed.framework.j2d.TestPanelJ2D;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class RobotTest extends TestbedTest {
+    @Override
+    public void initTest(boolean deserialized) {
+        setTitle("RobotTest");
+        getWorld().setGravity(new Vec2());
+        createWorld();
+    }
+
+    abstract public void createWorld();
+
+    @Override
+    public synchronized void step(TestbedSettings settings) {
+        super.step(settings);
+    }
+
+    @Override
+    public String getTestName() {
+        return "RobotTest";
+    }
+
+    public void start(){
+        TestbedModel model = new TestbedModel() {
+            @Override
+            public TestbedSettings getSettings() {
+                return new TestbedSettings() {
+                    private ArrayList<TestbedSetting> settings;
+
+                    {
+                        settings = new ArrayList<TestbedSetting>();
+                    }
+
+                    @Override
+                    public List<TestbedSetting> getSettings() {
+                        return Collections.unmodifiableList(settings);
+                    }
+
+                    @Override
+                    public TestbedSetting getSetting(String argName) {
+                        return super.getSetting(argName);
+                    }
+                };
+            }
+        };
+        model.addCategory("Tests");
+        model.addTest(this);
+        TestbedPanel panel = new TestPanelJ2D(model);    // create our testbed panel
+        JFrame testbed = new TestbedFrame(model, panel, TestbedController.UpdateBehavior.UPDATE_CALLED); // put both into our testbed frame
+
+        testbed.setVisible(true);
+        testbed.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/main/java/robo2d/testbed/tests/move/MoveTest.java
@@ -0,0 +1,10 @@
+package robo2d.testbed.tests.move;
+
+import robo2d.testbed.RobotTest;
+
+public class MoveTest extends RobotTest {
+    @Override
+    public void createWorld() {
+        
+    }
+}
